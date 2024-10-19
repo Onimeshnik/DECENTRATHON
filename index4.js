1115d6f692b087ce9f934cd7afeb8ec03f105456
Onimeshnik <50239878+Onimeshnik@users.noreply.github.com>
2024-10-19 23:36:13 +0500

    Update index4.js

--- index4.js
+++ index4.js
@@ -2,7 +2,7 @@ Telegram.WebApp.ready();
 
 function chooseWay(wayID) {
     if (wayID == 1) {
-        window.location.href = 'https://noname5969.rf.gd/index.html';
+        window.location.href = 'https://noname5969.rf.gd/it.html';
     } else {
         alert('Выбери что-нибудь другое.');
     }
